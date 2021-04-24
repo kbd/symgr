b677a36b079224c3a9e2a426382479614c6bd90b
Keith Devens <keith@keithdevens.com>
2021-04-24 14:26:32 -0600

    Add dry-run mode; unify bless and normal linking

--- symgr.py
+++ symgr.py
@@ -37,12 +37,30 @@ class SymPath(type(Path())):  # type: ignore # https://stackoverflow.com/a/34116
     def ensure_parent_exists(self):
         return self.parent.mkdir(parents=True, exist_ok=True)
 
-    def safe_symlink_to(self, other):
+    def safe_symlink_to(self, other, bless=False, dry_run=False):
         """Ensure destination path exists, back up any existing file"""
-        log.info(f"Pointing {self} -> {other}")
-        self.ensure_parent_exists()
-        self.backup_if_file_exists()
-        self.symlink_to(other)
+        # other is from, self is to
+        d = f" ({dry_run=})" if dry_run else ''
+        me = self
+        if bless:
+            me = me.resolve_target(other.name).resolve()
+
+        log.info(f"{me} -> {other}{d}")
+
+        if dry_run:
+            return True
+
+        if me.handle_existing_symlink(other):
+            me.ensure_parent_exists()
+            me.backup_if_file_exists()
+            if bless:
+                copy2(other, me)
+                other.backup_if_file_exists()
+                other.symlink_to(me)
+            else:
+                me.symlink_to(other)
+
+        return True
 
     def handle_existing_symlink(self, to_path):
         """Delete existing symlink if exists and is wrong.
@@ -65,7 +83,7 @@ class SymPath(type(Path())):  # type: ignore # https://stackoverflow.com/a/34116
     def resolve_target(self, name):
         return self / name if self.is_dir() else self
 
-    def link_at(self, target):
+    def link_at(self, target, bless=False, dry_run=False):
         source = self.resolve()  # make absolute
         if not source.exists():
             log.info(f"{source} doesn't exist, skipping")
@@ -75,45 +93,31 @@ class SymPath(type(Path())):  # type: ignore # https://stackoverflow.com/a/34116
             log.debug(f"{target} is already a correct symlink; doing nothing")
             return True
 
-        assert source != target.resolve(), "source path must not equal target path"
+        if source == target.resolve():
+            log.critical("source path must not equal target path")
+            return False
 
-        if not args.no_ignore:
-            if source.is_ignored():
-                log.debug(f"{source} is ignored")
-                return
+        if bless and not target.exists():
+            log.critical(f"{target} must exist to bless it")
+            return False
+
+        if not args.no_ignore and source.is_ignored():
+            log.debug(f"{source} is ignored")
+            return False
 
         if source.is_system_file():
             log.debug(f"Ignoring system file {source}")
-            return
+            return False
 
         if source.is_dir():
             log.info(f"Linking directory {source} to {target}")
             for path in sorted(source.iterdir()):
                 final_target = target / path.relative_to(source)
-                path.link_at(final_target)
+                path.link_at(final_target, bless=bless, dry_run=dry_run)
         else:
-            final_path = target.resolve_target(source.name)
-            if final_path.handle_existing_symlink(source):
-                final_path.safe_symlink_to(source)
+            target.safe_symlink_to(source, bless=bless, dry_run=dry_run)
 
-    def bless(self, target):
-        """To bless means to copy a file to a path, then symlink it back to the
-        original location, backing up if necessary"""
-        assert self.exists(), "source must exist to bless it"
-        to_path = target.resolve_target(self.name)
-        to_path.ensure_parent_exists()
-        to_path.backup_if_file_exists()
-        copy2(self, to_path)
-        to_path.link_at(self)
-
-
-def main(args):
-    frm = SymPath(args.frm)
-    to = SymPath(args.to)
-    if args.bless:
-        frm.bless(to)
-    else:
-        frm.link_at(to)
+        return True
 
 
 if __name__ == "__main__":
@@ -123,9 +127,9 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--debug', action='store_true', help='Enable debug logging')
     parser.add_argument('-b', '--bless', action='store_true', help='"Bless" the from location to the destination directory. Copy it and link back to it')
     parser.add_argument('-I', '--no-ignore', action='store_true', help="Don't respect git ignore")
+    parser.add_argument('-D', '--dry-run', action='store_true', help="Don't take any action, show what would be done")
     args = parser.parse_args()
 
     level = logging.DEBUG if args.debug else logging.INFO
     logging.basicConfig(level=level, format="%(levelname)s:%(message)s")
-
-    sys.exit(main(args))
+    sys.exit(SymPath(args.frm).link_at(SymPath(args.to), bless=args.bless, dry_run=args.dry_run))
